7cacaf4027dbc9584d5bed3866060bb91c91a77b
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-06-28 10:46:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const display_number = document.querySelector(".number");
+const display_error = document.querySelector(".error");
 const btn_decrease = document.querySelector("#button-decrease");
 const btn_increase = document.querySelector("#button-increase");
 const btn = document.querySelector(".buttons");
@@ -8,8 +9,13 @@ btn_decrease.onclick = () => {
   console.log(counter);
   if (counter >= 0 && counter <= 9999) {
     if (counter != 0) {
+      display_number.style.display = "block";
+      display_error.style.display = "none";
       counter -= 1;
       display_number.innerHTML = counter;
+    } else {
+      display_number.style.display = "none";
+      display_error.style.display = "block";
     }
   }
 }
@@ -17,8 +23,13 @@ btn_increase.onclick = () => {
   console.log(counter);
   if (counter >= 0 && counter <= 9999) {
     if (counter != 9999) {
+      display_number.style.display = "block";
+      display_error.style.display = "none";
       counter += 1;
       display_number.innerHTML = counter;
+    } else {
+      display_number.style.display = "none";
+      display_error.style.display = "block";
     }
   }
 }
